80532d2fa658b6535a6da29dbced27774dbc4fb1
zhuqinfei <1586859762@qq.com>
2019-04-07 17:20:59 +0800

    添加下载

--- main.js
+++ main.js
@@ -1,18 +1,66 @@
 var yyy = document.getElementById('xxx');
 var context = yyy.getContext('2d');
+var lineWidth = 3
 
 aotoSetCanvasSize(yyy)
 
 listenToUser(yyy)
 
+//当前使用时画笔or橡皮擦
 var eraserEnabled = false
+pen.onclick = function () {
+    eraserEnabled = false
+    pen.classList.add('active')
+    eraser.classList.remove('active')
+}
 eraser.onclick = function () {
     eraserEnabled = true
-    actions.className = 'actions x'
+    eraser.classList.add('active')
+    pen.classList.remove('active')
 }
-brush.onclick = function () {
-    eraserEnabled = false
-    actions.className = 'actions'
+
+//三种画笔颜色
+red.onclick = function () {
+    context.strokeStyle = "red"
+    red.classList.add('active')
+    green.classList.remove('active')
+    blue.classList.remove('active')
+}
+green.onclick = function () {
+    context.strokeStyle = "green"
+    green.classList.add('active')
+    red.classList.remove('active')
+    blue.classList.remove('active')
+}
+blue.onclick = function () {
+    context.strokeStyle = "blue"
+    blue.classList.add('active')
+    green.classList.remove('active')
+    red.classList.remove('active')
+}
+
+//画笔粗细
+thin.onclick = function () {
+    lineWidth = 3
+}
+thick.onclick = function () {
+    lineWidth = 5
+}
+
+//清空屏幕
+clear.onclick=function(){
+    context.clearRect(0,0,yyy.width,yyy.height)
+}
+
+//保存图画
+download.onclick=function(){
+    var url=yyy.toDataURL("image/png")
+    var a=document.createElement('a')
+    document.body.appendChild(a)
+    a.href=url
+    a.download='我的画画'
+    a.target='_blank'
+    a.click()
 }
 
 /*******/
@@ -37,9 +85,8 @@ function drawCircle(x, y, radius) {
 }
 function drawLine(x1, y1, x2, y2) {
     context.beginPath()
-    context.strokeStyle = "black"
     context.moveTo(x1, y1)  // 起点
-    context.lineWidth = 3
+    context.lineWidth = lineWidth
     context.lineTo(x2, y2)  //终点
     context.stroke()
     context.closePath()
